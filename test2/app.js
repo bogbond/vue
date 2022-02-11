f6160418eff66e686c4548ea06581ff3d4669a55
Bohdan Bondar <43843880+bogbond@users.noreply.github.com>
2022-02-11 19:35:15 +0100

    ver test1

--- test2/app.js
+++ test2/app.js
@@ -4,7 +4,8 @@ const App = {
 			placeholder: "Enter text here ..",
 			title: "Task List",
 			inputValue: '',
-			notes: ['note_1']
+			notes: ['note_1'],
+			change: true
 		}
 	},
 	methods: {
@@ -14,15 +15,26 @@ const App = {
 		addNewNote(){
 			if(this.inputValue !==''){
 				this.notes.push(this.inputValue)
-				this.inputValue = ''	
+				this.inputValue = ''
+				// this.console.log(change)	
 			}
 		},
 		toUpperCase(item){
-return item.toUpperCase()
+			return item.toUpperCase()
 		},
 		removeNote(i){
 			this.notes.splice(i, 1)
 
+		},
+		changeNote(idx){
+			console.log("changeBTN" + idx),
+			this.change = false
+			console.log(this.change)
+		},
+		changeNoteB(idx){
+			console.log("changeBTNback" + idx),
+			this.change = true
+			console.log(this.change)
 		}
 	}
 }
